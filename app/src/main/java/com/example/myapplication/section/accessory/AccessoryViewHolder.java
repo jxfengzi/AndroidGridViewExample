1f32467e392a7f2973a52c6c873577f6ad055d9d
ouyang <ouyangchengfeng@xiaomi.com>
2019-07-02 14:21:51 +0800

    实现点击缩放效果

--- app/src/main/java/com/example/myapplication/section/accessory/AccessoryViewHolder.java
+++ app/src/main/java/com/example/myapplication/section/accessory/AccessoryViewHolder.java
@@ -1,25 +1,74 @@
 package com.example.myapplication.section.accessory;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.ViewPropertyAnimatorListener;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
 
-public class AccessoryViewHolder extends RecyclerView.ViewHolder {
+public class AccessoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
 
+    private static final String TAG = "AccessoryViewHolder";
+    private final ConstraintLayout layout;
     private final TextView textViewRoom;
     private final TextView textViewName;
 
     public AccessoryViewHolder(View view) {
         super(view);
+        layout = view.findViewById(R.id.accessoryLayout);
         textViewRoom = view.findViewById(R.id.textViewRoom);
         textViewName = view.findViewById(R.id.textViewName);
+
+        layout.setClickable(true);
+        layout.setOnClickListener(this);
+
+        layout.setLongClickable(true);
+        layout.setOnLongClickListener(this);
     }
 
     public void onBind(Accessory accessory) {
         textViewRoom.setText(accessory.room());
         textViewName.setText(accessory.name());
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "onClick");
+        this.scaleUp();
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        Log.d(TAG, "onLongClick");
+        return true;
+    }
+
+    private void scaleUp() {
+        Log.d(TAG, "scaleUp");
+        ViewCompat.animate(layout)
+                .setDuration(200)
+                .scaleX(1.1f)
+                .scaleY(1.1f)
+                .withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        scaleDown();
+                    }
+                })
+                .start();
+    }
+
+    private void scaleDown() {
+        Log.d(TAG, "scaleDown");
+        ViewCompat.animate(layout)
+                .setDuration(200)
+                .scaleX(1f)
+                .scaleY(1f)
+                .start();
     }
 }
--- app/src/main/java/com/example/myapplication/section/scene/SceneViewHolder.java
+++ app/src/main/java/com/example/myapplication/section/scene/SceneViewHolder.java
@@ -1,22 +1,69 @@
 package com.example.myapplication.section.scene;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.core.view.ViewCompat;
+import androidx.core.view.ViewPropertyAnimatorListener;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
 
-public class SceneViewHolder extends RecyclerView.ViewHolder {
+public class SceneViewHolder extends RecyclerView.ViewHolder  implements View.OnClickListener, View.OnLongClickListener {
 
+    private static final String TAG = "AccessoryViewHolder";
     private final TextView textView;
 
     public SceneViewHolder(View view) {
         super(view);
         textView = view.findViewById(R.id.textView);
+
+        textView.setClickable(true);
+        textView.setOnClickListener(this);
+
+        textView.setLongClickable(true);
+        textView.setOnLongClickListener(this);
     }
 
     public void onBind(Scene scene) {
         textView.setText(scene.name());
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "onClick");
+        this.scaleUp();
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        Log.d(TAG, "onLongClick");
+        return true;
+    }
+
+    private void scaleUp() {
+        Log.d(TAG, "scaleUp");
+        ViewCompat.animate(textView)
+                .setDuration(200)
+                .scaleX(1.1f)
+                .scaleY(1.1f)
+                .withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        scaleDown();
+                    }
+                })
+                .start();
+    }
+
+    private void scaleDown() {
+        Log.d(TAG, "scaleDown");
+        ViewCompat.animate(textView)
+                .setDuration(200)
+                .scaleX(1f)
+                .scaleY(1f)
+                .start();
     }
 }
